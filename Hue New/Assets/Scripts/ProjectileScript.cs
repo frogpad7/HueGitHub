9ce2b90be757b5b362e95f3e3ceb674d9d49498d
Charles Ian Doucet <cdouce6@lsu.edu>
2014-03-20 17:57:38 -0500

    First Commit
    Hopefully GitHub likes me today....

---
+++ Hue New/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour 
+{
+	public Sprite red;
+	public Sprite orange;
+	public Sprite yellow;
+	public Sprite green;
+	public Sprite blue;
+	public Sprite purple;
+
+	private Transform camera;
+	private float lifetime = 0;
+	private bool grenadeExploding;
+	// Use this for initialization
+	void Start() 
+	{
+		grenadeExploding = false;
+		camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+		if (gameObject.tag == "Grenade") 		{ Debug.Log ("Grenade create"); lifetime = Time.time + 3; }
+		else if (gameObject.tag == "Orange") 	{ Debug.Log ("Orange create"); lifetime = Time.time + 10; }
+		else if (gameObject.tag == "Yellow") 	{ Debug.Log ("Platform create"); lifetime = Time.time + 6; }
+		else if (gameObject.tag == "Green") 	{ Debug.Log ("Dash create"); lifetime = Time.time + 2; }
+		else if (gameObject.tag == "Blue") 		{ Debug.Log ("Bubble create"); lifetime = Time.time + 1; }
+		else if (gameObject.tag == "Purple") 	{ Debug.Log ("Glove create"); lifetime = Time.time + 1; }
+		else lifetime = Time.time + 10;
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (gameObject.tag == "Projectile"||
+		    gameObject.tag == "Blue"||gameObject.tag == "Purple") { /*do nothing*/}
+
+		else if (gameObject.tag == "Orange") 
+		{
+			Debug.Log ("Orange particle hit something");
+			//if (impacted object is water) freeze it 
+			if(col.gameObject.tag == "Enemy")
+			{
+				this.rigidbody2D.isKinematic = true;
+				Debug.Log("Orange: Hit emeny");
+				//this.transform.position = col.gameObject.transform.position;
+				//Destroy (gameObject);
+			}
+			else if(col.gameObject.tag != "Player")
+			{
+				this.rigidbody2D.isKinematic = true;
+			}
+		}
+
+		/*if (col.gameObject.tag == "Player")
+		{
+			//player shot himself condition
+			Debug.Log ("player hit own bullet");
+		} */
+
+		//make grenades go boom
+		if (gameObject.tag == "Grenade")
+		{
+			if(col.gameObject.tag == "Enemy")
+			{
+				this.gameObject.rigidbody2D.isKinematic = true;
+				lifetime = Time.time + (float)0.5;
+				grenadeExploding = true;
+			}
+			//GameObject.FindWithTag("Backdrop").GetComponent<SplatterScript>().Splat (red,transform.position);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (transform.position.x > camera.transform.position.x+50f || transform.position.x < camera.transform.position.x-50f)
+		{
+			Debug.Log ("grenade out of range!");
+			Destroy (gameObject);
+		}
+
+		//weapon lifetime is up
+		if (lifetime > Time.time && grenadeExploding) 
+		{
+			Collider2D[] boom = Physics2D.OverlapCircleAll (gameObject.transform.position, 20f);
+			int l = boom.GetLength (0);
+			
+			for (int i=0; i<l; i++) 
+			{
+				//Debug.Log ("Boom");
+				GameObject hit = boom [i].gameObject;
+				if (hit.rigidbody2D != null) 
+				{
+					if (hit.rigidbody2D.gameObject.tag == "Player")
+					{
+						hit.rigidbody2D.AddForce ((hit.transform.position - gameObject.transform.position) * 20f);
+						Debug.Log ("Pushing!");
+					}
+					else if (hit.rigidbody2D.gameObject.tag == "Enemy")
+					{
+						Debug.Log("grenade blast hit enemy!");
+						Destroy(hit.rigidbody2D.gameObject);
+					}
+				}
+			}
+		}
+		else if (lifetime < Time.time && grenadeExploding)
+		{
+			Debug.Log ("grenade out of life");
+			Destroy (this.gameObject);
+		}
+		else if (lifetime < Time.time && !grenadeExploding) 
+		{ 
+			//spawn color splat
+			if (gameObject.tag == "Grenade") 
+			{
+				lifetime = Time.time + (float)0.5;
+				grenadeExploding = true;
+
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (red, transform.position);
+			} 
+			else if (gameObject.tag == "Orange")
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (orange, transform.position);
+			else if (gameObject.tag == "Yellow")
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (yellow, transform.position);
+			else if (gameObject.tag == "Green")
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (green, transform.position);
+			else if (gameObject.tag == "Blue")
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (blue, transform.position);
+			else if (gameObject.tag == "Purple")
+				GameObject.FindWithTag ("Backdrop").GetComponent<SplatterScript> ().Splat (purple, transform.position);
+
+			if(!grenadeExploding) 
+			{
+				Debug.Log ("Player Weapon Dissipated"); 
+				Destroy (this.gameObject); 
+			}
+		} 
+	}
+	
+}
