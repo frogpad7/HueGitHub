4b12188c9b5dd7d56bafcdb478661480d8e3f4b7
Charles Ian Doucet <cdouce6@lsu.edu>
2014-03-23 11:38:49 -0500

    SVN Update stuff
    here are some files both Andrew and Ethan added before I updated.

--- Hue New/Assets/Scripts/PlayerScript.cs
+++ Hue New/Assets/Scripts/PlayerScript.cs
@@ -61,9 +61,9 @@ public class PlayerScript : MonoBehaviour
 		else if (col.gameObject.tag == "Green")  	c = 4;
 		else if (col.gameObject.tag == "Blue")   	c = 5;
 		else if (col.gameObject.tag == "Purple") 	c = 6;
-
 		anim.SetInteger ("Color", c);
-		
+
+		//change audio track based on the color
 		if (c != color) 
 		{
 			GameObject.FindWithTag("Audio").GetComponent<AudioScript>().ChangeTrack(c);
@@ -106,14 +106,13 @@ public class PlayerScript : MonoBehaviour
 	{
 		//checks to see if player can jump
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRad, whatIsGround);
-
 		rBlocked = Physics2D.OverlapCircle(rightCheck.position, groundRad, whatIsGround);
+
 		//make sure we can't triple jump
 		if (grounded && isDoubleJumping)  isDoubleJumping = false;
 
 		float move = Input.GetAxis("Horizontal");
-		if(rBlocked && move > 0)
-			move = 0;
+		if(rBlocked && move > 0) move = 0;
 
 		float moveY = Input.GetAxis("Vertical");
 		float s = move;
@@ -141,19 +140,15 @@ public class PlayerScript : MonoBehaviour
 		if (move > 0 && !facingRight) Flip ();
 		else if (move < 0 && facingRight)	Flip ();
 	}
-		
-	void Update()
+
+	void inputProc_devCheats()
 	{
-		//debugging color change
+		//color change cheat
 		if (Input.GetKeyDown (KeyCode.LeftControl)) 
 		{
 			color= (color % 6) + 1;
 			anim.SetInteger("Color",color);
 		}
-
-		if (grounded) flying = false;
-		else if (!grounded && (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)))
-			flying = true;
 
 		//double jump Space key control
 		if (!isDoubleJumping && Input.GetKeyDown (KeyCode.Space)) 
@@ -162,22 +157,29 @@ public class PlayerScript : MonoBehaviour
 			isDoubleJumping = true;
 			//Debug.Log (isDoubleJumping);
 		}
+	}
+
+	void inputProc_jump()
+	{
 		//jumping from the ground, and reset the double jump flag
-		else if (grounded && Input.GetKeyDown(KeyCode.Space)) 
+		if (grounded && Input.GetKeyDown(KeyCode.Space)) 
 		{
 			rigidbody2D.velocity= new Vector2(rigidbody2D.velocity.x,0f);
 			rigidbody2D.AddForce (new Vector2 (0, 3500f));
 			isDoubleJumping = false;
 		} 
 
+		if (grounded) flying = false;
+		else if (!grounded && (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)))
+			flying = true;
+	}
+
+	void ability_bubbleShield()
+	{
 		//when we have a bubble active, this allows hover
 		if (color == 5 && bubbleShield && !grounded && flying && bubbleTime >= Time.time) 
 		{
-			Debug.Log ("FLOATING");
-			//Debug.Log (gameObject.rigidbody2D.velocity.y);
-			//gameObject.rigidbody2D.isKinematic = true;
 			gameObject.rigidbody2D.gravityScale = 0;
-
 		}
 		else if (bubbleShield && bubbleTime < Time.time) 
 		{
@@ -189,245 +191,214 @@ public class PlayerScript : MonoBehaviour
 			gameObject.rigidbody2D.gravityScale = 1;
 		}
 		else gameObject.rigidbody2D.gravityScale = 1;
+	}
 
-		//else(grounded)
-		//		isDoubleJumping = false;
+	void ability_Red()
+	{
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 firePos;
+			if(facingRight) 	firePos = transform.position + new Vector3(2,0,0);
+			else				firePos = transform.position + new Vector3(-2,0,0);
+			
+			Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+		}
+		else if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 firePos = transform.position + new Vector3(0,2,0);
+			Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(0,60);
+		}
+		else if(facingRight)
+		{ 
+			Vector3 firePos = transform.position + new Vector3(2,0,0);
+			Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(30,30);
+		}	
+		else
+		{
+			Vector3 firePos = transform.position + new Vector3(-2,0,0);
+			Rigidbody2D fireObj = Instantiate (grenade, firePos, Quaternion.Euler (new Vector3(0,0,0))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(-30,30);
+
+		}
+		cooldown = 3  + Time.time;
+	}
+
+	void ability_Orange()
+	{
+		if (Input.GetKey (KeyCode.DownArrow) && !grounded) 
+		{
+				Vector3 firePos = transform.position + new Vector3 (0, -2, 0);
+				Rigidbody2D fireObj = Instantiate (freeze, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+				fireObj.velocity = new Vector2 (0, -15);
+					
+		}
+		else if (Input.GetKey (KeyCode.UpArrow))
+		{
+				Vector3 firePos = transform.position + new Vector3 (0, 2, 0);
+				Rigidbody2D fireObj = Instantiate (freeze, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+				fireObj.velocity = new Vector2 (0, 15);
+		}
+		else if (facingRight)
+		{
+				Vector3 firePos = transform.position + new Vector3 (2, 0, 0);
+				Rigidbody2D fireObj = Instantiate (freeze, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+				fireObj.velocity = new Vector2 (15, 0);
+		}
+		else 
+		{
+				Vector3 firePos = transform.position + new Vector3 (-2, 0, 0);
+				Rigidbody2D fireObj = Instantiate (freeze, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+				fireObj.velocity = new Vector2 (-15, 0);
+		}
+		cooldown = 1 + Time.time;
+	}
+
+	void ability_Yellow()
+	{
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 firePos;
+			firePos = transform.position + new Vector3(0,-2,0);
+			Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+
+		}
+		else if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 firePos = transform.position + new Vector3(0,5,0);
+			Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+		}
+		else if(facingRight)
+		{
+			Vector3 firePos = transform.position + new Vector3(4,0,0);
+			Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
+			fireObj.isKinematic = true;					
+		}	
+		else
+		{
+			Vector3 firePos = transform.position + new Vector3(-4,0,0);
+			Rigidbody2D fireObj = Instantiate (paintWall, firePos, Quaternion.Euler (new Vector3(0,0,90f))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+		}
+		cooldown = Time.time + 3;
+	}
+
+	void ability_Green()
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			//raycast logic
+			Vector2 fwd = transform.TransformDirection (new Vector2 (0, 2));
+			RaycastHit2D cast = Physics2D.Raycast (transform.position + new Vector3 (0, 2, 0), fwd, 15);
+			if (cast != null && cast.collider != null) 
+				if (cast.collider.tag == "Enemy") Destroy (cast.collider.gameObject);
+
+			Vector3 firePos = transform.position + new Vector3 (0, 2, 0);
+			Rigidbody2D fireObj = Instantiate (dash, firePos, Quaternion.Euler (new Vector3 (0, 0, 90f))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+			transform.position += new Vector3 (0, 15, 0);
+		} 
+		else if (facingRight) 
+		{
+			//raycast logic
+			Vector2 fwd = transform.TransformDirection (new Vector2 (0, 2));
+			RaycastHit2D cast = Physics2D.Raycast (transform.position + new Vector3 (2, 0, 0), fwd, 15);
+			if (cast != null && cast.collider != null) 
+				if (cast.collider.tag == "Enemy") Destroy (cast.collider.gameObject);
+						
+			Vector3 firePos = transform.position + new Vector3 (2, 0, 0);
+			Rigidbody2D fireObj = Instantiate (dash, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+			transform.position += new Vector3 (15, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.DownArrow) && !grounded) 
+		{
+			//raycast logic
+			Vector2 fwd = transform.TransformDirection (new Vector2 (0, 2));
+			RaycastHit2D cast = Physics2D.Raycast (transform.position + new Vector3 (0, -2, 0), fwd, 15);
+			if (cast != null && cast.collider != null)
+				if (cast.collider.tag == "Enemy") Destroy (cast.collider.gameObject);
+											
+			Vector3 firePos = transform.position + new Vector3 (0, -2, 0);
+			Rigidbody2D fireObj = Instantiate (dash, firePos, Quaternion.Euler (new Vector3 (0, 0, 90f))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+			transform.position += new Vector3 (0, -15, 0);
+		} 
+		else if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			//raycast logic
+			Vector2 fwd = transform.TransformDirection (new Vector2 (0, 2));
+			RaycastHit2D cast = Physics2D.Raycast (transform.position + new Vector3 (-2, 0, 0), fwd, 15);
+			if (cast != null && cast.collider != null)
+				if (cast.collider.tag == "Enemy") Destroy (cast.collider.gameObject);
+
+			Vector3 firePos = transform.position + new Vector3 (-2, 0, 0);
+			Rigidbody2D fireObj = Instantiate (dash, firePos, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+			fireObj.isKinematic = true;
+			Destroy (fireObj.gameObject, .5f);
+			transform.position += new Vector3 (-15, 0, 0);
+
+		}
+		cooldown = Time.time + 3;
+	}
+
+	void ability_Blue()
+	{
+		//activate the bubble shield
+		if(!bubbleShield && cooldown <= Time.time && bubbleTime < Time.time)
+		{
+			Vector3 pos = (Vector3)transform.position;
+			bubbleShield = Instantiate (bubbleSH, pos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+			bubbleTime = Time.time + 5;
+			cooldown = Time.time + 10;
+		}
+	}
+
+	void ability_Purple()
+	{
+		if(grounded && Input.GetKey(KeyCode.DownArrow)) rigidbody2D.AddForce (new Vector2 (0, 2000f));
+		
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 firePos = transform.position + new Vector3(0,2,0);
+			Rigidbody2D fireObj = Instantiate(glove, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(0,15);
+			cooldown = Time.time + 1;
+		}
+		else if(facingRight && Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 firePos = transform.position + new Vector3(2,0,0);
+			Rigidbody2D fireObj = Instantiate(glove, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(15,0);
+			cooldown = Time.time + 1;
+		}	
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 firePos = transform.position + new Vector3(-2,0,0);
+			Rigidbody2D fireObj = Instantiate (glove, firePos, Quaternion.Euler (new Vector3(0,0,180f))) as Rigidbody2D;
+			fireObj.velocity = new Vector2(-15,0);
+			cooldown = Time.time + 1;
+		}
+	}
+		
+	void Update()
+	{
+		//debugging color change & double jump
+		inputProc_devCheats();
+		inputProc_jump();
+		ability_bubbleShield();
 
 		//ability button control
-		if (Input.GetKeyDown (KeyCode.LeftShift)) 
+		if (Input.GetKeyDown (KeyCode.LeftShift) && cooldown <= Time.time) 
 		{
-			//RED: GRENADE
-			if(color == 1)
-			{
-				if(Input.GetKey(KeyCode.DownArrow) && cooldown <= Time.time)
-				{
-					Vector3 firePos;
-					if(facingRight) 	firePos = transform.position + new Vector3(2,0,0);
-					else				firePos = transform.position + new Vector3(-2,0,0);
-					
-					Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					cooldown = 3  + Time.time;
-				}
-				else if(Input.GetKey(KeyCode.UpArrow) && cooldown <= Time.time)
-				{
-					Vector3 firePos = transform.position + new Vector3(0,2,0);
-					Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(0,60);
-					cooldown = 3 + Time.time;
-				}
-				else if(facingRight && cooldown <= Time.time)
-				{ 
-					Vector3 firePos = transform.position + new Vector3(2,0,0);
-					Rigidbody2D fireObj = Instantiate(grenade, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(30,30);
-					cooldown = 3  + Time.time;
-				}	
-				else if (cooldown <= Time.time)
-				{
-					Vector3 firePos = transform.position + new Vector3(-2,0,0);
-					Rigidbody2D fireObj = Instantiate (grenade, firePos, Quaternion.Euler (new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(-30,30);
-					cooldown = 3  + Time.time;
-				}
-			}
-
-			//ORANGE: FREEZE
-			if(color==2 && cooldown <= Time.time)
-			{
-				if(Input.GetKey(KeyCode.DownArrow)&&!grounded)
-				{
-					Vector3 firePos = transform.position + new Vector3(0,-2,0);
-					Rigidbody2D fireObj = Instantiate(freeze, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(0,-15);
-					cooldown = 1  + Time.time;	
-				}
-				else if(Input.GetKey(KeyCode.UpArrow))
-				{
-					Vector3 firePos = transform.position + new Vector3(0,2,0);
-					Rigidbody2D fireObj = Instantiate(freeze, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(0,15);
-					cooldown = 1  + Time.time;
-				}
-				else if(facingRight)
-				{
-					Vector3 firePos = transform.position + new Vector3(2,0,0);
-					Rigidbody2D fireObj = Instantiate(freeze, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(15,0);
-					cooldown = 1  + Time.time;
-				}	
-				else
-				{
-					Vector3 firePos = transform.position + new Vector3(-2,0,0);
-					Rigidbody2D fireObj = Instantiate (freeze, firePos, Quaternion.Euler (new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(-15,0);
-					cooldown = 1  + Time.time;
-				}
-			}
-
-			//YELLOW: The walls
-			if(color == 3 && cooldown <= Time.time)
-			{
-				if(Input.GetKey(KeyCode.DownArrow))
-				{
-					Vector3 firePos;
-					firePos = transform.position + new Vector3(0,-2,0);
-					Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					cooldown = Time.time + 3;
-				}
-				else if(Input.GetKey(KeyCode.UpArrow))
-				{
-					Vector3 firePos = transform.position + new Vector3(0,5,0);
-					Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					cooldown = Time.time + 3;
-				}
-				else if(facingRight)
-				{
-					Vector3 firePos = transform.position + new Vector3(4,0,0);
-					Rigidbody2D fireObj = Instantiate(paintWall, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.isKinematic = true;					
-					cooldown = Time.time + 3;
-				}	
-				else
-				{
-					Vector3 firePos = transform.position + new Vector3(-4,0,0);
-					Rigidbody2D fireObj = Instantiate (paintWall, firePos, Quaternion.Euler (new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					cooldown = Time.time + 3;				
-				}
-			}
-	
-			//GREEN: the warp-dash
-			if(color == 4 && cooldown <= Time.time)
-			{
-				if(Input.GetKey(KeyCode.UpArrow))
-				{
-					//raycast logic
-					Vector2 fwd = transform.TransformDirection(new Vector2(0,2));
-					RaycastHit2D cast = Physics2D.Raycast(transform.position + new Vector3(0,2,0), fwd, 15);
-					if (cast != null && cast.collider != null) 
-					{
-						if(cast.collider.tag == "Enemy")
-						{
-							Destroy(cast.collider.gameObject);
-							Debug.Log("Green: target hit via raycast");
-						}
-						else Debug.Log ("Green: non-enemy collision");
-					}
-
-					Vector3 firePos = transform.position + new Vector3(0,2,0);
-					Rigidbody2D fireObj = Instantiate(dash, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					transform.position += new Vector3(0,15,0);
-					cooldown = Time.time + 3;
-				}
-				else if(facingRight)
-				{
-					//raycast logic
-					Vector2 fwd = transform.TransformDirection(new Vector2(0,2));
-					RaycastHit2D cast = Physics2D.Raycast(transform.position + new Vector3(2,0,0), fwd, 15);
-					if (cast != null && cast.collider != null) 
-					{
-						if(cast.collider.tag == "Enemy")
-						{
-							Destroy(cast.collider.gameObject);
-							Debug.Log("Green: target hit via raycast");
-						}
-						else Debug.Log ("Green: non-enemy collision");
-					}
-
-					Vector3 firePos = transform.position + new Vector3(2,0,0);
-					Rigidbody2D fireObj = Instantiate(dash, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					transform.position += new Vector3(15,0,0);
-					cooldown = Time.time + 3;
-				}
-				else if(Input.GetKey(KeyCode.DownArrow) && !grounded)
-				{
-					//raycast logic
-					Vector2 fwd = transform.TransformDirection(new Vector2(0,2));
-					RaycastHit2D cast = Physics2D.Raycast(transform.position + new Vector3(0,-2,0), fwd, 15);
-					if (cast != null && cast.collider != null) 
-					{
-						if(cast.collider.tag == "Enemy")
-						{
-							Destroy(cast.collider.gameObject);
-							Debug.Log("Green: target hit via raycast");
-						}
-						else Debug.Log ("Green: non-enemy collision");
-					}
-
-					Debug.Log ("Down-Dash");
-					Vector3 firePos = transform.position + new Vector3(0,-2,0);
-					Rigidbody2D fireObj = Instantiate(dash, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					transform.position += new Vector3(0,-15,0);
-					cooldown = Time.time + 3;
-				}
-				else if(Input.GetKey(KeyCode.LeftArrow))
-				{
-					//raycast logic
-					Vector2 fwd = transform.TransformDirection(new Vector2(0,2));
-					RaycastHit2D cast = Physics2D.Raycast(transform.position + new Vector3(-2,0,0), fwd, 15);
-					if (cast != null && cast.collider != null) 
-					{
-						if(cast.collider.tag == "Enemy")
-						{
-							Destroy(cast.collider.gameObject);
-							Debug.Log("Green: target hit via raycast");
-						}
-						else Debug.Log ("Green: non-enemy collision");
-					}
-
-					Vector3 firePos = transform.position + new Vector3(-2,0,0);
-					Rigidbody2D fireObj = Instantiate(dash, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.isKinematic = true;
-					Destroy (fireObj.gameObject,.5f);
-					transform.position += new Vector3(-15,0,0);
-					cooldown = Time.time + 3;
-				}
-			}
-
-			//BLUE: the bubble
-			if(color==5)
-			{
-				//activate the bubble shield
-				if(!bubbleShield && cooldown <= Time.time && bubbleTime < Time.time)
-				{
-					Vector3 pos = (Vector3)transform.position;
-					bubbleShield = Instantiate (bubbleSH, pos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					bubbleTime = Time.time + 5;
-					cooldown = Time.time + 10;
-				}
-			}
-			
-			//PURPLE
-			if(color==6)
-			{
-				if(grounded && Input.GetKey(KeyCode.DownArrow)) rigidbody2D.AddForce (new Vector2 (0, 2000f));
-
-				if(Input.GetKey(KeyCode.UpArrow))
-				{
-					Vector3 firePos = transform.position + new Vector3(0,2,0);
-					Rigidbody2D fireObj = Instantiate(glove, firePos, Quaternion.Euler(new Vector3(0,0,90f))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(0,15);
-					cooldown = Time.time + 1;
-				}
-				else if(facingRight && Input.GetKey(KeyCode.RightArrow))
-				{
-					Vector3 firePos = transform.position + new Vector3(2,0,0);
-					Rigidbody2D fireObj = Instantiate(glove, firePos, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(15,0);
-					cooldown = Time.time + 1;
-				}	
-				else if (Input.GetKey(KeyCode.LeftArrow))
-				{
-					Vector3 firePos = transform.position + new Vector3(-2,0,0);
-					Rigidbody2D fireObj = Instantiate (glove, firePos, Quaternion.Euler (new Vector3(0,0,180f))) as Rigidbody2D;
-					fireObj.velocity = new Vector2(-15,0);
-					cooldown = Time.time + 1;
-				}
-			}
+			if(color == 1) ability_Red();
+			else if(color == 2) ability_Orange();
+			else if(color == 3) ability_Yellow();
+			else if(color == 4) ability_Green();
+			else if(color == 5) ability_Blue();
+			else if(color == 6) ability_Purple();
 		}
 	}
 
