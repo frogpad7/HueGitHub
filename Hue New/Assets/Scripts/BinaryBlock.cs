dcdc5e16d4c773f9ae74ebee867dbf17868a0333
ethancaraway <ecaraw1@tigers.lsu.edu>
2014-04-21 20:20:36 -0500

    Level 4 Art

--- Hue New/Assets/Scripts/BinaryBlock.cs
+++ Hue New/Assets/Scripts/BinaryBlock.cs
@@ -3,54 +3,56 @@ using System.Collections;
 
 public class BinaryBlock : MonoBehaviour {
 	
-	float respawnTotal = 10;
-	float respawnTimer = 0;
+	//float respawnTotal = 10;
+	//float respawnTimer = 0;
 
-	Vector3 initialPosition;
-	public Vector3 destinationParent = new Vector3(0, 0, 0);
-	Vector3 destinationPosition;
+	public bool present;
 
-	bool destination = false;
+	public Sprite isPresent;
+	public Sprite notPresent;
+
+	public GameObject other;
 
 	// Use this for initialization
 	void Start () { 
-		initialPosition = this.transform.position;
-		destinationPosition = new Vector3 (destinationParent.x + this.transform.localPosition.x, destinationParent.y + this.transform.localPosition.y, 0);
+	
 	}
 
 	//Detects player leaving block
 	void OnCollisionExit2D(Collision2D col) {
 		//Check if collision with the player
 		if (col.gameObject.tag == "Player") {
-			//Check if in the initial position or destination position
-			if (destination == false) {
-				//Move to destination position
-				this.transform.position = destinationPosition;
-				destination = true;
-			} else {
-				//Move to initial position
-				this.transform.position = initialPosition;
-				destination = false;
+			//Check if the block is present
+			if (present == true) {
+				//Switch this block to not present
+				present = false;
+				this.gameObject.GetComponent<SpriteRenderer>().sprite = notPresent;
+				this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+				//Switch other block to present
+				other.gameObject.GetComponent<BinaryBlock>().present = true;
+				other.gameObject.GetComponent<SpriteRenderer>().sprite = other.gameObject.GetComponent<BinaryBlock>().isPresent;
+				other.gameObject.GetComponent<BoxCollider2D>().enabled = true;
 			}
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//Check if in the destination position
-		if (destination == true) {
-			//Check if respawn timer has run out
-			if (respawnTimer >= respawnTotal) {
-				//Move back to initial position
-				this.transform.position = initialPosition;
-				destination = false;
-			
-				//Reset respawn timer
-				respawnTimer = 0;
-			} else {
-				//Increase respawn timer
-				respawnTimer += 1 * Time.deltaTime;
-			}
-		}
+//		//Check if in the destination position
+//		if (destination == true) {
+//			//Check if respawn timer has run out
+//			if (respawnTimer >= respawnTotal) {
+//				//Move back to initial position
+//				this.transform.position = initialPosition;
+//				destination = false;
+//			
+//				//Reset respawn timer
+//				respawnTimer = 0;
+//			} else {
+//				//Increase respawn timer
+//				respawnTimer += 1 * Time.deltaTime;
+//			}
+//		} 
 	}
 }
