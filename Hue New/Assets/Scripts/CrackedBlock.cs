920273b56544b5146f4ce5f26bdae2ff4205597c
ethancaraway <ecaraw1@tigers.lsu.edu>
2014-05-06 20:32:50 -0500

    Cracked Block Reset Function Added
    This was only need in two places it turns out

--- Hue New/Assets/Scripts/CrackedBlock.cs
+++ Hue New/Assets/Scripts/CrackedBlock.cs
@@ -3,10 +3,28 @@ using System.Collections;
 
 public class CrackedBlock : MonoBehaviour {
 
+	Sprite initSprite;
+
+	//Initialize initial sprite for reset
+	void Start () {
+		initSprite = this.gameObject.GetComponent<SpriteRenderer> ().sprite;
+	}
+
 	//Detects for player collision to enter the delete countdown
 	void OnCollisionEnter2D(Collision2D col) {
 		if (col.gameObject.tag == "Player") {
 			this.gameObject.GetComponent<Animator> ().enabled = true;
+		}
+	}
+
+	//Reset blocks
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			this.renderer.enabled = true;
+			this.collider2D.enabled = true;
+			this.gameObject.GetComponent<Animator> ().enabled = false;
+			this.gameObject.GetComponent<Animator> ().Play("Cracking", 0, 0f);
+			this.gameObject.GetComponent<SpriteRenderer> ().sprite = initSprite;
 		}
 	}
 
